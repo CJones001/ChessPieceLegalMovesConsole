0cadf666e62de490b62944932ddc5e7c9902132b
CJones001 <147557670+CJones001@users.noreply.github.com>
2024-01-17 11:56:52 -0700

    Add project files.

---
+++ ChessBoardConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChessBoardConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ChessBoardModel/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoardModel
+{
+    internal class Cell
+    {
+        // Row and Column for the Cells location on the grdi
+        public int RowNumber { get; set; }
+        public int ColumnNumber { get; set; }
+
+        // T/F; is the chess piece on this cell?
+        public bool CurrentlyOccupied { get; set; }
+
+        // T/F; is this cell legal for the chess piece to move to?
+        public bool LegalNextMove { get; set; }
+    }
+}
